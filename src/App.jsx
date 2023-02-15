1328cdfd5448682c94bd4f2d1b1be94eac95a010
siraj <sirajbhutta50@gmail.com>
2023-02-15 22:44:56 +0500

    docs: page data

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,16 @@ import user from './assets/user.png'
 import card1 from './assets/card1.png'
 import card2 from './assets/card2.png'
 import card3 from './assets/card3.png'
-
 import './App.css'
 
+/* 
+todo: add data
+---------
+? sections
+- nav
+- header
+---------
+*/
 function App() {
 
   return (
@@ -30,13 +37,12 @@ function Nav() {
         <div className='nav'>
        <div className='container'>
           <div className="navitem">
-            <p className="navlink">siraj</p>
-            <p className="navlink">contact</p>
+            <a className="navlink">siraj</a>
+            <a href="#cont" className="navlink">contact</a>
           </div>
         </div>
       </div>
-      </div>
-      
+      </div> 
     </>
   )
 }
@@ -48,7 +54,7 @@ function Header() {
       <div className='header'>
           <p className="headlg">hi, i am siraj.<br /> A web developer</p>
           <div className="headbottom">
-            <p className="para text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minus architecto porro eos itaque numquam esse quia consectetur molestias officiis.</p>
+            <p className="para text">Frontend developer and design system developer skilled with Javascript and React js.Mostly I use Javascript, React js, Html, CSS, Styled-components, Tailwind, Bootstrap, and Material UI </p>
             <img className="headimg" src={user} alt="image" />
           </div>  
         </div>
@@ -65,12 +71,12 @@ function Work() {
           <p className="headlg workheading">Work.</p>
           <div className="workcontainer">
             <div className="workupper">
-              <p className="navlink">Project Name</p>
-              <p className="navlink">live preview</p>
+              <p className="navlink">UI Dashborad</p>
+              <a className="navlink">live preview</a>
             </div>
             <img className="workimg" src={img1} alt="image" />
             <div className="worklower">
-              <p className="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p className="text">Build with react js and styled-components</p>
               < HiOutlineArrowNarrowRight className="workicon"/>
             </div>
           </div>  
@@ -91,19 +97,19 @@ function Process() {
             <div className="card" style={{'backgroundImage':`url('${card1}')`}}>
               <div className="cardcontent">
                 <p className="headsm">Idea</p>
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing. </p>
+                <p className="text">Always starts with hand drawn sketch</p>
               </div>
             </div>
             <div className="card" style={{'backgroundImage':`url('${card2}')`}}>
               <div className="cardcontent">
                 <p className="headsm">Design</p>
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <p className="text">Crafting beautifull ui from sketch</p>
               </div>
             </div>
             <div className="card" style={{'backgroundImage':`url('${card3}')`}}>
               <div className="cardcontent">
                 <p className="headsm">develop</p>
-                <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <p className="text">Developing whole web page with react js and styled-components</p>
               </div>
             </div>
           </div>
@@ -116,14 +122,13 @@ function Process() {
 function Contact() {
   return (
     <>
-      <div className='container'>  
+      <div id='cont' className='container'>  
         <div className="contact">
           <div className="contactlinks">
-            <p className="text">Lorem ipsum dolor sit.</p>
-            <p className="text">Lorem ipsum dolor sit.</p>
+            <p className="text">sirajbhutta50@gmail.com</p>
           </div>
           <p className="headlg">lets talk?</p>
-          <p className="text right">Lorem ipsum dolor sit.</p>
+          <p className="text right">@github</p>
         </div>
        </div>
     </>
